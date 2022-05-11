39fcec99c5d6ded3907124abdad681b9f48965e8
Breno Prado <breno.o.prado@hotmail.com>
2022-05-11 16:39:45 -0300

    Primeira etapa realizada :turtle:

--- src/average.js
+++ src/average.js
@@ -10,7 +10,21 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {};
+const average = (arr) => {
+let sum = 0;
+if (arr.length === 0) {
+  return undefined;
+}
+  for (let index = 0; index < arr.length; index += 1) {
+    sum += arr[index];
+    if (typeof arr[index] !== 'number') {
+      return undefined;
+    }
+  }
+  if (typeof sum === 'number') {
+    const media = sum / arr.length;
+    return Math.round(media);
+  }
+};
 
 module.exports = average;
